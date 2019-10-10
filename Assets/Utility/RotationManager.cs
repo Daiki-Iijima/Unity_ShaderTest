7534381faa7a95484927c8f09ad5dd96c2d327f0
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-10-11 01:25:11 +0900

    Update

---
+++ Assets/Utility/RotationManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObj;
+
+    public float OffSetHeight;
+    public float RotateSpeed;
+
+    private Vector3 targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = targetObj.transform.position;
+        targetPos = new Vector3(
+            targetPos.x,
+            targetPos.y + OffSetHeight,
+            targetPos.z);
+    }
+
+    private float value = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        value += RotateSpeed / 100;
+        this.transform.LookAt(targetPos, Vector3.up);
+        this.transform.position = new Vector3(Mathf.Sin(value), 0.5f, Mathf.Cos(value));
+    }
+}
